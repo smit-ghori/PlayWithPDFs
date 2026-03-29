01552d8717f086afd0ee287f1db81982afc288af
Smit-Ghori <smitghori83@gmail.com>
2026-03-29 13:24:28 +0530

    fix: replace googletrans to avoid cgi error

--- routes/translate.py
+++ routes/translate.py
@@ -1,15 +1,19 @@
 import os
 from flask import Blueprint, render_template, request, redirect, url_for
 from utils.file_utils import save_uploaded_files
-from googletrans import LANGUAGES, Translator
+from deep_translator import GoogleTranslator
 
 translate_bp = Blueprint("translate", __name__)
 
 UPLOAD_FOLDER = os.environ.get("UPLOAD_FOLDER", "uploads")
+
+# ✅ get all supported languages dynamically
+LANGUAGES = GoogleTranslator().get_supported_languages(as_dict=True)
 
 @translate_bp.route("/translate", methods=["GET", "POST"])
 @translate_bp.route("/translate/", methods=["GET", "POST"])
 def translate():
     if request.method == "POST":
         print("Hitted successfully")
+
     return render_template("translate.html", languages=LANGUAGES)
