9b744b26fa7d50efad8619c694087a12a05b867d
Smit-Ghori <smitghori83@gmail.com>
2026-05-24 21:54:45 +0530

    add replay option

--- routes/contact_us.py
+++ routes/contact_us.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, redirect, render_template, request, url_for
 from flask_mail import Message
 from extensions import mail
+import os
 
 contact_us_bp = Blueprint("contact_us", __name__)
 
@@ -11,19 +12,17 @@ def contact_us():
     if request.method == "POST":
 
         name = request.form.get("name")
-
         email = request.form.get("email")
-
         subject = request.form.get("subject")
-
         message = request.form.get("message")
 
         # EMAIL SEND
 
         msg = Message(
             subject=f"PlayWithPDFs Contact: {subject}",
-            sender="smitghori83@gmail.com",
-            recipients=["smitghori83@gmail.com"],
+            sender=os.environ.get("MAIL_USERNAME"),  # your Gmail account
+            recipients=[os.environ.get("MAIL_USERNAME")],  # must be a list
+            reply_to=email,  # user’s email, so reply goes to them
         )
 
         msg.body = f"""
@@ -41,6 +40,8 @@ Message:
 
         mail.send(msg)
 
-        return render_template("contact_us.html", success=True)
+        return redirect(url_for("contact_us.contact_us", success="1"))
 
-    return render_template("contact_us.html")
+    success = request.args.get("success") == "1"
+
+    return render_template("contact_us.html", success=success)
